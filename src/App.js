7344975bc3df0f9ceade927b9d9e6cd5e6bf5a5a
Edi Carlos <edi_csilva@hotmail.com>
2023-04-26 17:42:56 -0300

    ajustes para aula

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
-function App() {
-  return (
-    <h1>Olá</h1>
-  );
+import React, { Component} from 'react';
+import './App.css';
+
+class App extends Component {
+
+  render() {
+    return (
+      <h1>Olá</h1>
+    );
+  }
+
 }
 
 export default App;

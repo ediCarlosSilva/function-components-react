633d5d259cb3f5f97794ef882dcf29304ff7196b
Edi Carlos <edi_csilva@hotmail.com>
2023-04-27 16:46:13 -0300

    todos os componentes com materialUI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 import FormularioCadastro from './components/FormularioCadastro';
-import { Container } from '@mui/material';
+import { Container, Typography } from '@mui/material';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 class App extends Component {
 
   render() {
     return (
       <Container component="article" maxWidth="sm">
-        <h1>Formulário de Cadastro</h1>
+        <Typography variant="h3" component="h1" align="center">Formulário de Cadastro</Typography>
         <FormularioCadastro />
       </Container>
     );
--- src/components/FormularioCadastro/index.jsx
+++ src/components/FormularioCadastro/index.jsx
@@ -1,21 +1,24 @@
 import React from 'react';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
+import { TextField, Button, Switch, FormControlLabel } from '@mui/material';
 
 export default function FormularioCadastro() {
     return (
         <form>
             <TextField id="nome" fullWidth label="nome" variant='outlined' margin="normal" />
 
-            <TextField id="sobrenome" fullWidth label="Sobrenome" variant='outlined' margin="normal"/>
+            <TextField id="sobrenome" fullWidth label="Sobrenome" variant='outlined' margin="normal" />
 
             <TextField id="CPF" fullWidth label="CPF" variant='outlined' margin="normal" />
 
-            <label htmlFor="">Promoções</label>
-            <input type="checkbox" />
+            <FormControlLabel
+                label="Promoções"
+                control={<Switch name="promocoes" defaultChecked />}
+            />
 
-            <label htmlFor="">Novidades</label>
-            <input type="checkbox" />
+            <FormControlLabel 
+                label="Novidades" 
+                control={<Switch name="novidades" defaultChecked />}
+            />
 
             <Button type="submit" variant="contained" color="primary">Cadastrar</Button>
         </form>
